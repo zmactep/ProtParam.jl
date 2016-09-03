afa76e00711330611ce2e2201cea52210977320a
Tatiana Malygina <merlettaia@gmail.com>
2016-09-03 23:54:12 +0300

    more tests for src/functions.jl

--- test/functions_tests.jl
+++ test/functions_tests.jl
@@ -13,7 +13,8 @@ import ProtParam:
     absorbance,
     atom_composition,
     number_of_atoms,
-    aliphatic_index
+    aliphatic_index,
+    ProtParamType
 
 test_data = [
     (
@@ -22,15 +23,25 @@ test_data = [
         Dict(
             "negativecount" => 7,
             "positivecount" => 10,
-            #"half_life" => ,
+            "half_life" => Dict(
+                "Mammalian, in vitro" => "0.8 hour",
+                "Yeast, in vivo" => "10 min",
+                "E.Coli, in vivo" => "10 hour"
+            ), # this dict is used to test half_life function with `make_string` parameter set to true
             "instability_index" => 32.99,
             "stability" => "stable",
             "isoelectric_point" => 8.86,
             "gravy" => -0.454,
-            #"extinction_coeff" => 
+            "extinction_coeff" => (36900,37025), 
             "molecular_weight" => 12984.44,
-            #"absorbance" =>
-            #"atom_composition" =>
+            "absorbance" => (2.842, 2.851),
+            "atom_composition" => Dict(
+                ATOM_S => 5,
+                ATOM_C => 579,
+                ATOM_H => 866,
+                ATOM_N => 150,
+                ATOM_O => 181
+            ),
             "number_of_atoms" => 1781,
             "aliphatic_index" => 51.68
         )
@@ -41,15 +52,25 @@ test_data = [
         Dict(
             "negativecount" => 41,
             "positivecount" => 24,
-            #"half_life" => 
+            "half_life" => Dict(
+                "Mammalian, in vitro" => "30 hour",
+                "Yeast, in vivo" => ">20 hour",
+                "E.Coli, in vivo" => ">10 hour"
+            ), # this dict is used to test half_life function with `make_string` parameter set to true
             "instability_index" => 45.15,
             "stability" => "unstable",
             "isoelectric_point" => 4.57,
             "gravy" => -0.095,
-            #"extinction_coeff" => 
+            "extinction_coeff" => (15930,16305), 
             "molecular_weight" => 28768.78,
-            #"absorbance" =>
-            #"atom_composition" =>
+            "absorbance" => (0.554, 0.567),
+            "atom_composition" => Dict(
+                ATOM_S => 16,
+                ATOM_C => 1257,
+                ATOM_H => 2020,
+                ATOM_N => 328,
+                ATOM_O => 408
+            ),
             "number_of_atoms" => 4029,
             "aliphatic_index" => 94.87
         )
@@ -60,27 +81,44 @@ test_data = [
         Dict(
             "negativecount" => 1,
             "positivecount" => 4,
-            #"half_life" => 
+            "half_life" => Dict(
+                "Mammalian, in vitro" => "1 hour",
+                "Yeast, in vivo" => "2 min",
+                "E.Coli, in vivo" => "2 min"
+            ), # this dict is used to test half_life function with `make_string` parameter set to true
             "instability_index" => 44.64,
             "stability" => "unstable",
             "isoelectric_point" => 10.05,
             "gravy" => -0.090,
-            #"extinction_coeff" => 
+            "extinction_coeff" => (0, 0),
             "molecular_weight" => 2250.72,
-            #"absorbance" =>
-            #"atom_composition" =>
+            "absorbance" => (0, 0),
+            "atom_composition" => Dict(
+                ATOM_S => 3,
+                ATOM_C => 93,
+                ATOM_H => 168,
+                ATOM_N => 30,
+                ATOM_O => 28
+            ),
             "number_of_atoms" => 322,
             "aliphatic_index" => 97.50
         )
     )
 ]
 
+aa1 = test_data[1][2]
+println(atom_composition(aa1))
+#println(show(half_life(aa1)))
 for (name, sequence, data) in test_data
     facts(string("Run tests for ", name)) do
         for (function_name, expected_result) in data
             result = eval(Expr(:call, symbol(function_name), sequence))
             if isa(result, Number)
                 @fact result --> roughly(expected_result, 0.01)
+            elseif isa(result, Tuple)
+                for (u, v) in zip(result, expected_result)
+                    @fact u --> (isa(v, Number) ? roughly(v, 0.01) : v)
+                end
             else
                 @fact result --> expected_result
             end
