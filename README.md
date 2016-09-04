ef4ec980c8b99c0c9a0ff8971d53f6a54aab82cb
lacemaker <latticetower@users.noreply.github.com>
2016-09-04 21:17:21 +0300

    tests for src/functions.jl (#2)
    * update sample output in README.MD
    
    * add ugliest file with tests for function (only pI test works for now - others has incorrect numbers). Can't update now since expasy service is not available from airport wifi
    
    * changes in functions_tests.jl - made it more compact (but not all functions from src/functions.jl are tested yet)
    
    * fixed typo in half_life
    
    * fixed type in returned Dict
    
    * more tests for src/functions.jl
    
    * add description for testing cycle in test/functions_tests.jl

--- README.md
+++ README.md
@@ -19,62 +19,62 @@ protparam(rituximab)
 
 ```
 ProtParam
-       	Query protein sequence:
-       		QVQLQQPGAELVKPGASVKMSCKASGYTFTSYNMHWVKQTPGRGLEWIGAYPGNGDTSYN
-       		QKFKGKATLTADKSSSTAYMQLSSLTSEDSAVYYCARSTYYGGDWYFNVWAGTTVTVSA
+	Query protein sequence:
+		QVQLQQPGAELVKPGASVKMSCKASGYTFTSYNMHWVKQTPGRGLEWIGAYPGNGDTSYN
+		QKFKGKATLTADKSSSTAYMQLSSLTSEDSAVYYCARSTYYGGDWYFNVWAGTTVTVSA
 
-       	Theoretical pI:				0
-       	Molecular weight:      			12984.442140000005
-       	Number of amino acids in protein:      	119
-       	Number of atoms in protein:    		1781
+	Theoretical pI:				8.86279296875
+	Molecular weight:			12984.442140000005
+	Number of amino acids in protein:	119
+	Number of atoms in protein:		1781
 
-       	Total number of negatively charged residues (Asp + Glu):       	7
-       	Total number of positively charged residues (Arg + Lys):       	10
+	Total number of negatively charged residues (Asp + Glu):	7
+	Total number of positively charged residues (Arg + Lys):	10
 
-       	Extinction coefficient, assuming all pairs of Cys residues form cystines:
-       		E:     	37025
-       		Abs:   	2.8514894672248112
-       	Extinction coefficient, assuming all Cys residues are reduced:
-       		E:     	36900
-       		Abs:   	2.841862561528576
+	Extinction coefficient, assuming all pairs of Cys residues form cystines:
+		E:	37025
+		Abs:	2.8514894672248112
+	Extinction coefficient, assuming all Cys residues are reduced:
+		E:	36900
+		Abs:	2.841862561528576
 
-       	Half-life:
-       		Estimated half-life in mammalian reticulocytes, in vitro:      	0.8 hour
-       		Estimated half-life in yeast, in vivo: 				10 min
-       		Estimated half-life in Escherichia coli, in vivo:      		>10 hour
+	Half-life:
+		Estimated half-life in mammalian reticulocytes, in vitro:	0.8 hour
+		Estimated half-life in yeast, in vivo:				10 min
+		Estimated half-life in Escherichia coli, in vivo:		>10 hour
 
-       	The instability index (II):    	0
-       	This classifies the protein as stable.
+	The instability index (II):	32.99495798319327
+	This classifies the protein as stable.
 
-       	Aliphatic index of a protein:  		51.68067226890756
-       	Grand Average of Hydropathy (GRAVY):   	-0.4537815126050419
+	Aliphatic index of a protein:		51.68067226890756
+	Grand Average of Hydropathy (GRAVY):	-0.4537815126050419
 
-       	Amino acids composition:
-       		A      	11
-       		C      	2
-       		D      	4
-       		E      	3
-       		F      	3
-       		G      	12
-       		H      	1
-       		I      	1
-       		K      	8
-       		L      	6
-       		M      	3
-       		N      	4
-       		P      	4
-       		Q      	7
-       		R      	2
-       		T      	12
-       		S      	14
-       		V      	8
-       		W      	4
-       		Y      	10
+	Amino acids composition:
+		A	11
+		C	2
+		D	4
+		E	3
+		F	3
+		G	12
+		H	1
+		I	1
+		K	8
+		L	6
+		M	3
+		N	4
+		P	4
+		Q	7
+		R	2
+		T	12
+		S	14
+		V	8
+		W	4
+		Y	10
 
-       	Atoms composition:
-       		S      	5
-       		C      	579
-       		H      	866
-       		N      	150
-       		O      	181
+	Atoms composition:
+		O	181
+		H	866
+		C	579
+		N	150
+		S	5
 ```
---
+++ test/functions_tests.jl
@@ -0,0 +1,127 @@
+# This test set ensures that methods from `src/functions.jl` behave the same way as they should
+# This is checked by comparing results of particular methods with corresponding output from ExPASy.
+import ProtParam: 
+    negativecount,
+    positivecount,
+    half_life,
+    instability_index,
+    stability,
+    isoelectric_point, 
+    gravy,
+    extinction_coeff,
+    molecular_weight,
+    absorbance,
+    atom_composition,
+    number_of_atoms,
+    aliphatic_index,
+    ProtParamType
+
+test_data = [
+    (
+        "rituximab",
+        aa"QVQLQQPGAELVKPGASVKMSCKASGYTFTSYNMHWVKQTPGRGLEWIGAYPGNGDTSYNQKFKGKATLTADKSSSTAYMQLSSLTSEDSAVYYCARSTYYGGDWYFNVWAGTTVTVSA",
+        Dict(
+            "negativecount" => 7,
+            "positivecount" => 10,
+            "half_life" => Dict(
+                "Mammalian, in vitro" => "0.8 hour",
+                "Yeast, in vivo" => "10 min",
+                "E.Coli, in vivo" => "10 hour"
+            ), # this dict is used to test half_life function with `make_string` parameter set to true
+            "instability_index" => 32.99,
+            "stability" => "stable",
+            "isoelectric_point" => 8.86,
+            "gravy" => -0.454,
+            "extinction_coeff" => (36900,37025), 
+            "molecular_weight" => 12984.44,
+            "absorbance" => (2.842, 2.851),
+            "atom_composition" => Dict(
+                ATOM_S => 5,
+                ATOM_C => 579,
+                ATOM_H => 866,
+                ATOM_N => 150,
+                ATOM_O => 181
+            ),
+            "number_of_atoms" => 1781,
+            "aliphatic_index" => 51.68
+        )
+    ), 
+    (   
+        "PCNA",
+        aa"MFEARLVQGSILKKVLEALKDLINEACWDISSSGVNLQSMDSSHVSLVQLTLRSEGFDTYRCDRNLAMGVNLTSMSKILKCAGNEDIITLRAEDNADTLALVFEAPNQEKVSDYEMKLMDLDVEQLGIPEQEYSCVVKMPSGEFARICRDLSHIGDAVVISCAKDGVKFSASGELGNGNIKLSQTSNVDKEEEAVTIEMNEPVQLTFALRYLNFFTKATPLSSTVTLSMSADVPLVVEYKIADMGHLKYYLAPKIEDEEGS",
+        Dict(
+            "negativecount" => 41,
+            "positivecount" => 24,
+            "half_life" => Dict(
+                "Mammalian, in vitro" => "30 hour",
+                "Yeast, in vivo" => ">20 hour",
+                "E.Coli, in vivo" => ">10 hour"
+            ), # this dict is used to test half_life function with `make_string` parameter set to true
+            "instability_index" => 45.15,
+            "stability" => "unstable",
+            "isoelectric_point" => 4.57,
+            "gravy" => -0.095,
+            "extinction_coeff" => (15930,16305), 
+            "molecular_weight" => 28768.78,
+            "absorbance" => (0.554, 0.567),
+            "atom_composition" => Dict(
+                ATOM_S => 16,
+                ATOM_C => 1257,
+                ATOM_H => 2020,
+                ATOM_N => 328,
+                ATOM_O => 408
+            ),
+            "number_of_atoms" => 4029,
+            "aliphatic_index" => 94.87
+        )
+    ),
+    (
+        "PCNA fragment",
+        aa"RCDRNLAMGVNLTSMSKILK",
+        Dict(
+            "negativecount" => 1,
+            "positivecount" => 4,
+            "half_life" => Dict(
+                "Mammalian, in vitro" => "1 hour",
+                "Yeast, in vivo" => "2 min",
+                "E.Coli, in vivo" => "2 min"
+            ), # this dict is used to test half_life function with `make_string` parameter set to true
+            "instability_index" => 44.64,
+            "stability" => "unstable",
+            "isoelectric_point" => 10.05,
+            "gravy" => -0.090,
+            "extinction_coeff" => (0, 0),
+            "molecular_weight" => 2250.72,
+            "absorbance" => (0, 0),
+            "atom_composition" => Dict(
+                ATOM_S => 3,
+                ATOM_C => 93,
+                ATOM_H => 168,
+                ATOM_N => 30,
+                ATOM_O => 28
+            ),
+            "number_of_atoms" => 322,
+            "aliphatic_index" => 97.50
+        )
+    )
+]
+
+# Following code cycles through examples given in test_data array, for each of 
+# them compares values of functions (each function given as a key in Dict) with
+# corresponding expected values (which were taken from ExPASy output).
+for (name, sequence, data) in test_data
+    facts(string("Run tests for ", name)) do
+        for (function_name, expected_result) in data
+            result = eval(Expr(:call, Symbol(function_name), sequence))
+            if isa(result, Number)
+                @fact result --> roughly(expected_result, 0.01)
+            elseif isa(result, Tuple)
+                for (u, v) in zip(result, expected_result)
+                    @fact u --> (isa(v, Number) ? roughly(v, 0.01) : v)
+                end
+            else
+                @fact result --> expected_result
+            end
+        end
+    end
+end
